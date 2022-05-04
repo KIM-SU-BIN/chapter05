b82561620698440d91f705ae6279489ce15ccb80
subintoto <subintoto@naver.com>
2022-05-04 18:27:32 +0900

    0503 ppt15

--- src/com/javaex/ex03/Person.java
+++ src/com/javaex/ex03/Person.java
@@ -21,6 +21,32 @@ public class Person {
 	}
 
 	
+	//gs
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
 
 	
 	//메소드-일반
